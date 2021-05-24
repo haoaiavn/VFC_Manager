f94560b9fed559d6dfd117e70654ff1a1b1938f5
Le Thanh Hao <hao.aiavn@gmail.com>
2021-05-25 02:48:17 +0700

    Fix path icon android/app/src/main/assets/ and Set option for tabBarIcon

--- src/navigations/mainNavigator/MainNavigator.js
+++ src/navigations/mainNavigator/MainNavigator.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import NewsFeedStack from '../stacks/NewFeedStack';
 import StationStack from '../stacks/StationStack';
@@ -12,11 +14,49 @@ const Tab = createBottomTabNavigator();
 export default function MainNavigator(){
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="NewsFeed" component={NewsFeedStack} options={{ tabBarBadge: 3 }} />
-                <Tab.Screen name="Station" component={StationStack} />
-                <Tab.Screen name="Notification" component={NotificationStack} />
-                <Tab.Screen name="Setting" component={SettingStack} />
+            <Tab.Navigator
+                initialRouteName="NewsFeed"
+                tabBarOptions={{
+                    activeTintColor: '#4EC4FF',
+                    showLabel: false,
+                }}>
+                <Tab.Screen 
+                    name="NewsFeed"
+                    component={NewsFeedStack}
+                    options={{
+                        tabBarLabel: "NewsFeed",
+                        tabBarIcon: ({focused, color, size}) => (
+                            <Ionicons name="home" size={size} color={color} />
+                          ),
+                    }} />
+                <Tab.Screen
+                    name="Station"
+                    component={StationStack}
+                    options={{
+                        tabBarLabel: "Station",
+                        tabBarIcon: ({focused, color, size}) => (
+                            <Ionicons name="people" size={size} color={color} />
+                        ),
+                    }} />
+                <Tab.Screen
+                    name="Notification"
+                    component={NotificationStack}
+                    options={{
+                        tabBarBadge: 3,
+                        tabBarLabel: "Notification",
+                        tabBarIcon: ({focused, color, size}) => (
+                            <Ionicons name="notifications" size={size} color={color} />
+                        ),
+                    }} />
+                <Tab.Screen
+                    name="Setting"
+                    component={SettingStack}
+                    options={{
+                        tabBarLabel: "Setting",
+                        tabBarIcon: ({focused, color, size}) => (
+                            <Ionicons name="settings" size={size} color={color} />
+                          ),
+                    }} />
             </Tab.Navigator>
         </NavigationContainer>        
     );
--- src/screens/mainScreen/NewsFeedScreen.js
+++ src/screens/mainScreen/NewsFeedScreen.js
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import {Text} from 'react-native';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 export default function NewsFeedScreen(){
     return (
-        <Text>NewsFeed Screen</Text>
+        <Text>
+            <Ionicons name="home" size={30}/>
+        </Text>
     );
 }
